086836e6246c0d8d65ea22cc3aed234e5c5748c7
Aishwaryaschakote <aishwarya.s.chakote@gmail.com>
2025-03-24 16:28:26 +0530

    it's program1 in 24th march

--- day1.py
+++ day1.py
@@ -1,5 +1,19 @@
 # OPERATORS
 # ASSIGNMENT OPERATORS
 # BITWISE OPERATORS
+#bin() function
+a=13
+b=15
 
+print(bin(a))
+print(bin(b))
+
+print(a&b)
+print(bin(a&b))
+
+print(a|b)
+print(bin(a|b))
+
+print(a^b)
+print(bin(a^b))
 
