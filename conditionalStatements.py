8270564029bf838517608ff9e6c7a921fd845b63
Aishwaryaschakote <aishwarya.s.chakote@gmail.com>
2025-03-24 18:13:52 +0530

    it's 24th march conditionalStatements

---
+++ conditionalStatements.py
@@ -0,0 +1,28 @@
+# WAP check the eligibility criteria for voting
+# age=18
+# if(age>=18):
+#     print(" u are eligible for voting..!")
+# else:
+#     print(" u are not eligible for voting..!")
+#########################
+# WAP check the eligibility criteria for driving licence
+# age=44
+# if(age<16):
+#     print(" u are  not eligible for learning and driving licences..!")
+# elif((16<age) and (age<18)): # elif((16<=age<=18)):
+#     print(" u can apply for learning licence..!")
+# else:
+#     print(" u are  eligible for driving licence..!")
+############################
+
+marks=int(input("Emter your marks:"))
+if(90<=marks<=100):
+    print("A")
+elif(80<=marks<=90):
+    print("B")
+elif(70<=marks<=80):
+    print("C")
+elif(60<=marks<=70):
+    print("D")
+elif(marks<60):
+    print("FAIL")
--- day1.py
+++ day1.py
@@ -2,11 +2,14 @@
 # ASSIGNMENT OPERATORS
 # BITWISE OPERATORS
 #bin() function
-a=25
-b=27
+# a=25
+# b=27
 
 # print(bin(a))
 # print(bin(b))
+# num = 10
+# binary_str = bin(num)[2:]  # Remove the '0b' prefix by slicing the string
+# print(binary_str)
 
 # # print(a&b)
 # print(bin(a&b))
@@ -17,8 +20,20 @@ b=27
 # print(a^b)
 # print(bin(a^b))
 
-a=5
-b=10.5
-print(a+b)
-print(int(a+b))
+# a=5
+# b=10.5
+# print(a+b)
+# print(int(a+b))
 
+a=15
+print(a)
+a*=15
+print(a)
+a+=0.5
+print(a)
+a*=a
+print(a)
+a*a
+print(a)
+a**2
+print(a)
