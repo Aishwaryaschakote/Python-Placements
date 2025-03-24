55e28a1da70af4597a189d6a96301e943208827a
Aishwaryaschakote <aishwarya.s.chakote@gmail.com>
2025-03-24 17:12:55 +0530

    Added day1.py

--- day1.py
+++ day1.py
@@ -2,18 +2,23 @@
 # ASSIGNMENT OPERATORS
 # BITWISE OPERATORS
 #bin() function
-a=13
-b=15
+a=25
+b=27
 
-print(bin(a))
-print(bin(b))
+# print(bin(a))
+# print(bin(b))
 
-print(a&b)
-print(bin(a&b))
+# # print(a&b)
+# print(bin(a&b))
 
-print(a|b)
-print(bin(a|b))
+# # print(a|b)
+# print(bin(a|b))
 
-print(a^b)
-print(bin(a^b))
+# print(a^b)
+# print(bin(a^b))
+
+a=5
+b=10.5
+print(a+b)
+print(int(a+b))
 
